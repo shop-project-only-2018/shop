77977b315da7aa2a4e918f28e83620ec32698511
ybRS2018 <43371751+ybRS2018@users.noreply.github.com>
2018-10-29 16:59:02 +0300

    29.10.2018 #

---
+++ src/main/java/shop/mappers/customer/CustomerMapper.java
@@ -0,0 +1,15 @@
+package shop.mappers.customer;
+
+
+import org.mapstruct.Mapper;
+import shop.dtos.customer.CustomerDto;
+import shop.model.customer.Customer;
+
+@Mapper(componentModel = "spring")
+public interface CustomerMapper {
+
+    Customer getEntity(CustomerDto dto);
+
+    CustomerDto getDto(Customer entity);
+
+}
